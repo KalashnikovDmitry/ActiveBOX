1ffc17c4981bbd7064a4186d4a9b0a508f1c219b
unknown <slardakoo@gmail.com>
2024-04-28 23:30:39 +0300

    add swiper

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper(".swiper-testimonials", {
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    mousewhell: true,
+    keyboard: true,
+  });
