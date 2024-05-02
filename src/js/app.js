8096b76a7f0abbc912ec7d775d81cd11e397d2b7
ChuChucky <slardakoo@gmail.com>
2024-05-02 17:29:44 +0300

    update full

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,36 @@
 const swiper = new Swiper(".swiper-testimonials", {
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    mousewhell: true,
-    keyboard: true,
-  });
+  direction: "horizontal",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  mousewhell: true,
+  keyboard: true,
+  breakpoints: {
+    1024: {
+      direction: "horizontal",
+    } ,
+    320: { 
+      direction: "vertical", 
+    }
+  },
+});
+
+//  let checkbox = document.querySelectorAll('input[type=checkbox]');
+//   let op = document.querySelector("header-main-wrap");
+
+
+//       checkbox.forEach((v) => {
+//         if(v.checked) {
+            
+//           op.classList.add('active');
+
+    
+//         } else {
+//           op.classList.remove('active');
+//         }
+//     });
+     
+  
+    
+
